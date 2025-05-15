c8a780ed41c44791c6a19a96a7d8e004cbc637df
plgreenLIRU <p.l.green@liverpool.ac.uk>
2025-05-15 11:36:06 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ARX",
-    version="0.2.1",
+    version="0.3.0",
     author="P.L.Green",
     description="Auto-regressive machine learning with exogeneous inputs",
     packages=find_packages(),
