77622ee742f62461f0ee767fb9fca2b1bf107bc6
Pete Green <42935860+plgreenLIRU@users.noreply.github.com>
2025-05-20 11:37:02 +0000

    ready for review

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ARX",
-    version="0.3.0",
+    version="0.3.1",
     author="P.L.Green",
     description="Auto-regressive machine learning with exogeneous inputs",
     packages=find_packages(),
